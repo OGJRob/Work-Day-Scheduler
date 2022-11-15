0d69fb0717603e7a4ef526bdf3d438173c3ec575
OGJRob <John.Rob19@aol.com>
2022-11-14 23:23:35 -0600

    Cleaned up js, finalizing README

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
-  // TODO: Add code to display the current date in the header of the page.
-
+  
+  
   // https://www.youtube.com/watch?v=K9fjwq--WpU&ab_channel=CoderDost && https://zetcode.com/javascript/dayjs/
 const currentdayjs = dayjs();
 let day = currentdayjs.date();
@@ -28,15 +28,9 @@ function timeFrame () {
 
     let hourBlock = $(this).attr("id")
    const textValue= localStorage.getItem(hourBlock);
-   console.log("SPLIT", hourBlock.split("-"))
-   console.log("AFTER POP", hourBlock.split("-").pop())
-   console.log("TYPEOF",typeof hourBlock.split("-").pop())
    hourBlock= parseInt(hourBlock.split("-").pop())
 
    $(this).children(".description").val(textValue)
-
-   // console.log($(this).attr("id"))
-   // console.log(currentTime)
 
     if (hourBlock < currentTime) {
       $(this).addClass("past");
@@ -48,12 +42,3 @@ function timeFrame () {
   }
 )};
 timeFrame ();
-
-
-
-
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
