481f8be3acb99e15be6b458ff7e7298ac12b2bf5
OGJRob <John.Rob19@aol.com>
2022-11-14 22:58:53 -0600

    User input & hour functions now up and running

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,43 @@
   // TODO: Add code to display the current date in the header of the page.
 
   // https://www.youtube.com/watch?v=K9fjwq--WpU&ab_channel=CoderDost && https://zetcode.com/javascript/dayjs/
-const j = dayjs();
-let day = j.date();
-let month = j.month();
-let year = j.year();
+const currentdayjs = dayjs();
+let day = currentdayjs.date();
+let month = currentdayjs.month();
+let year = currentdayjs.year();
 
-let currentTime = moment().format("H");
-
-const currentDate = j.format("MMM DD, YYYY");
+const currentDate = currentdayjs.format("MMM DD, YYYY");
 
 let scheduleDate = document.getElementById("currentDay")
 scheduleDate.textContent = currentDate;
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+$(document).ready(function () {
 
+  $(".saveBtn").on("click", function () {
+    let text = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
 
+    localStorage.setItem(time, text);
+  })
+})
   
-  // TODO: Add code to apply the past, present, or future class to each time
+let currentTime = dayjs().format("H");
+
 function timeFrame () {
   $(".time-block").each(function() {
-    let hourBlock = parseInt($(this).attr("id"))
+
+    let hourBlock = $(this).attr("id")
+   const textValue= localStorage.getItem(hourBlock);
+   console.log("SPLIT", hourBlock.split("-"))
+   console.log("AFTER POP", hourBlock.split("-").pop())
+   console.log("TYPEOF",typeof hourBlock.split("-").pop())
+   hourBlock= parseInt(hourBlock.split("-").pop())
+
+   $(this).children(".description").val(textValue)
+
+   // console.log($(this).attr("id"))
+   // console.log(currentTime)
+
     if (hourBlock < currentTime) {
       $(this).addClass("past");
     } else if (hourBlock === currentTime) {
@@ -37,6 +48,9 @@ function timeFrame () {
   }
 )};
 timeFrame ();
+
+
+
 
 
   //
